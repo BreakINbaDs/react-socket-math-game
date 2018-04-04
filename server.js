f0d286dd8d9c71da7daf51dfa6a381dbcf6fcfb9
BreakINbaDs <shapovalromeo@gmail.com>
2018-04-04 19:04:31 +0300

    Pre-automated-testing version

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-
 const express = require('express')
 const http = require('http')
 const socketIO = require('socket.io')
-
+var round = 0;
+var rounds = [{round:1, question: "1+1=2?", answer: "true"}];
 // our localhost port
 const port = 4001
 
@@ -14,9 +14,49 @@ const server = http.createServer(app)
 // This creates our socket using the instance of the server
 const io = socketIO(server)
 
+// Function for questions generation
+function questionGen(round) {
+	var a = Math.floor((Math.random() * 10) + 1);
+	var b = Math.floor((Math.random() * 10) + 1);
+	var c = Math.floor((Math.random() * 10) + 1);
+	var k = Math.floor((Math.random() * 4) + 1);
+
+
+	if (k == 1) {
+		var answer = 'false';
+		var res = a*(c+b) + 1;
+		var question = a+'*('+c+'+'+b+')='+res+'?';
+	} else if (k == 2) {
+		var answer = 'true';
+		var res = (a+b)/c;
+		var question = '('+a+'+'+b+')/'+c+'='+res+'?';
+	} else if (k == 3) {
+		var answer = 'false';
+		var res = a*b/c - 1;
+		var question = a+'*'+b+'/'+c+'='+res+'?';
+	} else {
+		var answer = 'true';
+		var res = a+b+c;
+		var question = a+'+'+b+'+'+c+'='+res+'?';
+	}
+
+	var data = {round: round + 1, question: question, answer: answer };
+	return data;
+}
+
 io.on('connection', socket => {
-  console.log('User connected')
+  console.log('New client connected')
+  socket.emit('Initialization', rounds[rounds.length - 1]);
   
+  socket.on('give answer', (answer) => {
+    console.log('The right answer was given!', answer);
+    round = round + 1;
+    var data = questionGen(round);
+	io.sockets.emit('next round', data);
+	rounds.push(data);
+  })
+  
+  // disconnect is fired when a client leaves the server
   socket.on('disconnect', () => {
     console.log('user disconnected')
   })
--- socket-client/src/App.js
+++ socket-client/src/App.js
@@ -1,20 +1,100 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+// import packages
+import React, { Component } from 'react';
+import socketIOClient from 'socket.io-client';
+import ReactDOM from 'react-dom';
+const socket = socketIOClient( "http://localhost:4001");
+
+
+// Making the App component
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      answer: "",
+      score: 0,
+      question: "",
+      round: 0,
+      nextRound: false,
+      click: false
+    }
+  }
+
+  componentDidMount() {
+    socket.on('Initialization', (data) => {
+      this.setState({round: data.round, 
+        question: data.question, 
+        answer: data.answer});
+    });
+    socket.on('next round', (data) => {
+      this.setState({nextRound: true});
+      console.log('Next raund will start in 5 sec!');
+      // Add notification
+      const element = <h1>The right answer has been submited, 5 seconds are left till the new round!</h1>;
+      ReactDOM.render(element, document.getElementById('timer'));
+      // 5 sec timeout
+      setTimeout(function() {if (this.state.click) {this.setState({round: data.round, 
+        question: data.question, 
+        answer: data.answer,
+        nextRound: false,
+        click: false});
+      } else if (!this.state.click && this.state.score != 0){
+        this.setState({round: data.round, 
+        question: data.question, 
+        answer: data.answer,
+        score: this.state.score - 1,
+        nextRound: false,
+        click: false});
+      } else {
+        this.setState({round: data.round, 
+        question: data.question, 
+        answer: data.answer,
+        nextRound: false,
+        click: false});
+      }
+        this.refs.btn_true.style.display = 'block';
+        this.refs.btn_false.style.display = 'block';
+        // Remove notification
+        const element = <h1></h1>;
+        ReactDOM.render(element, document.getElementById('timer'));}.bind(this), 5000);
+    });
+  }
+
+   // method for emitting a socket.io event
+  sendAnswer = (answer) => { 
+    this.refs.btn_true.style.display = 'none';
+    this.refs.btn_false.style.display = 'none';
+
+    if (answer == this.state.answer && this.state.nextRound == false) {
+      socket.emit('give answer', this.state.answer);
+      this.setState({score: this.state.score + 1, click: true}); 
+
+    } else if (answer !== this.state.answer && this.state.score != 0) {
+      this.setState({score: this.state.score - 1, click: true});
+    } else {
+      this.setState({click: true});
+    }
+  }
+  
   render() {
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={{ textAlign: "center" }}>
+        <div id='info-container'>
+          <div id='round'>Round: {this.state.round}</div>
+          <div id='timer'></div>
+          <div id='score'>Score: {this.state.score}</div>
+        </div>
+        <div id='title'>Math Game</div>
+        <div id='question-container'>
+          <div>{this.state.question}</div>
+          <div id='btns'>
+            <button ref="btn_true" className='btn' onClick={() => this.sendAnswer('true')}>True</button>
+            <button ref="btn_false" className='btn' onClick={() => this.sendAnswer('false')}>False</button>
+          </div>
+        </div>
       </div>
-    );
+    )
   }
 }
 
