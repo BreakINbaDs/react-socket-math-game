55c4bd93572c64e2a20336bffed37e5b0826ad57
BreakINbaDs <shapovalromeo@gmail.com>
2018-04-05 08:56:52 +0300

    Tests added

---
+++ test/test.js
@@ -0,0 +1,50 @@
+var expect = require('chai').expect;
+var io     = require('socket.io-client');
+
+var app = require('../testServer/index');
+
+var socketUrl = 'http://localhost:4001';
+var options = {
+  transports: ['websocket'],
+  'force new connection': true
+};
+
+
+describe('Sockets', function () {
+  var client, client1, client2, client3;
+  it('First client should receive initialization data from server:', function (done) {
+    // Set up client connection
+    client = io.connect(socketUrl, options);
+    data = {round:1, question: "1+1=2?", answer: "true"};
+
+    // Set up event listener
+    client.on('Initialization', function(msg){
+      expect(msg.round).to.equal(data.round);
+      expect(msg.question).to.equal(data.question);
+      expect(msg.answer).to.equal(data.answer);
+      // Disconnect client
+      client.disconnect();
+      done();
+    });
+  });
+
+  it('All cients should be notified that round has been ended:', function (done) {
+    // Set up clients connections
+    client1 = io.connect(socketUrl, options);
+    client2 = io.connect(socketUrl, options);
+    client3 = io.connect(socketUrl, options);
+
+    client1.emit('give answer');
+    // Set up event listener
+    client2.on('next round', function (msg){
+      expect(msg).to.have.keys('round', 'question', 'answer');
+    });
+    client3.on('next round', function (msg){
+      expect(msg).to.have.keys('round', 'question', 'answer');
+    });
+    client1.disconnect();
+    client2.disconnect();
+    client3.disconnect();
+    done();
+  });
+});
